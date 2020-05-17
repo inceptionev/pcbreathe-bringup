da1e1211f8f1d271d1aef3ee6e43d86f1651ecbd
Edwin Chiu <edwin@soeffects.com>
2020-05-16 19:43:59 -0700

    added stepper driver support for homing and driving Marc-designed pinch valve

--- pcbreathe-bringup.ino
+++ pcbreathe-bringup.ino
@@ -11,6 +11,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Wire.h>
+#include <powerSTEP01ArduinoLibrary.h>
 
 
 //hardware definitions
@@ -27,6 +28,16 @@
 #define PIN_LED_Y PC14
 #define PIN_LED_G PC15
 const int chipSelect = PA15;  //SD card chip select
+// Pin definitions for the X-NUCLEO-IHM03A1 (stepper driver)
+#define nCS_PIN PB6
+#define STCK_PIN PC7
+#define nSTBY_nRESET_PIN PA9
+#define nBUSY_PIN PB5
+
+//Pinch valve motion settings
+#define STARTSTROKE 7000
+#define OPENPOS 200
+#define CLOSEDPOS 6200
 
 //i2c test device definitions
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -35,6 +46,8 @@ const int chipSelect = PA15;  //SD card chip select
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);  //instantiate display
 
+//instantiate stepper driver
+powerSTEP driver(0, nCS_PIN, nSTBY_nRESET_PIN);
 
 //test parameters
 #define BUZZER_VOL 5 //buzzer volume
@@ -79,6 +92,84 @@ void setup() {
   pinMode(PIN_BLOWER,OUTPUT);
   pinMode(PIN_SOLENOID,OUTPUT);
   pinMode(PIN_HEATER,OUTPUT);
+  //Stepper driver pins
+  pinMode(nSTBY_nRESET_PIN, OUTPUT);
+  pinMode(nCS_PIN, OUTPUT);
+  pinMode(MOSI, OUTPUT);
+  pinMode(MISO, OUTPUT);
+  pinMode(SCK, OUTPUT);
+
+  // Reset powerSTEP and set CS
+  digitalWrite(nSTBY_nRESET_PIN, HIGH);
+  digitalWrite(nSTBY_nRESET_PIN, LOW);
+  digitalWrite(nSTBY_nRESET_PIN, HIGH);
+  digitalWrite(nCS_PIN, HIGH);
+
+  // Start SPI
+  SPI.begin();
+  SPI.setDataMode(SPI_MODE3);
+
+  // Configure powerSTEP
+  driver.SPIPortConnect(&SPI); // give library the SPI port
+  
+  driver.configSyncPin(BUSY_PIN, 0); // use SYNC/nBUSY pin as nBUSY, 
+                                     // thus syncSteps (2nd paramater) does nothing
+                                     
+  driver.configStepMode(STEP_FS_128); // 1/128 microstepping, full steps = STEP_FS,
+                                // options: 1, 1/2, 1/4, 1/8, 1/16, 1/32, 1/64, 1/128
+                                
+  driver.setMaxSpeed(1000); // max speed in units of full steps/s 
+  driver.setFullSpeed(2000); // full steps/s threshold for disabling microstepping
+  driver.setAcc(2000); // full steps/s^2 acceleration
+  driver.setDec(2000); // full steps/s^2 deceleration
+  
+  driver.setSlewRate(SR_520V_us); // faster may give more torque (but also EM noise),
+                                  // options are: 114, 220, 400, 520, 790, 980(V/us)
+                                  
+  driver.setOCThreshold(8); // over-current threshold for the 2.8A NEMA23 motor
+                            // used in testing. If your motor stops working for
+                            // no apparent reason, it's probably this. Start low
+                            // and increase until it doesn't trip, then maybe
+                            // add one to avoid misfires. Can prevent catastrophic
+                            // failures caused by shorts
+  driver.setOCShutdown(OC_SD_ENABLE); // shutdown motor bridge on over-current event
+                                      // to protect against permanant damage
+  
+  driver.setPWMFreq(PWM_DIV_1, PWM_MUL_0_75); // 16MHz*0.75/(512*1) = 23.4375kHz 
+                            // power is supplied to stepper phases as a sin wave,  
+                            // frequency is set by two PWM modulators,
+                            // Fpwm = Fosc*m/(512*N), N and m are set by DIV and MUL,
+                            // options: DIV: 1, 2, 3, 4, 5, 6, 7, 
+                            // MUL: 0.625, 0.75, 0.875, 1, 1.25, 1.5, 1.75, 2
+                            
+  driver.setVoltageComp(VS_COMP_DISABLE); // no compensation for variation in Vs as
+                                          // ADC voltage divider is not populated
+                                          
+  driver.setSwitchMode(SW_USER); // switch doesn't trigger stop, status can be read.
+                                 // SW_HARD_STOP: TP1 causes hard stop on connection 
+                                 // to GND, you get stuck on switch after homing
+                                      
+  driver.setOscMode(INT_16MHZ); // 16MHz internal oscillator as clock source
+
+  // KVAL registers set the power to the motor by adjusting the PWM duty cycle,
+  // use a value between 0-255 where 0 = no power, 255 = full power.
+  // Start low and monitor the motor temperature until you find a safe balance
+  // between power and temperature. Only use what you need
+  driver.setRunKVAL(60); //2.8V in voltage mode for 2A max on 1.4ohm coils
+  driver.setAccKVAL(60);
+  driver.setDecKVAL(60);
+  driver.setHoldKVAL(32);
+
+  driver.setParam(ALARM_EN, 0x8F); // disable ADC UVLO (divider not populated),
+                                   // disable stall detection (not configured),
+                                   // disable switch (not using as hard stop)
+
+  driver.getStatus(); // clears error flags
+
+  //home the actuator
+  driver.move(REV, STARTSTROKE); // move into the stop
+  while(driver.busyCheck()); // wait fo the move to finish
+  driver.resetPos(); //establish home
 
   //Setup display (i2c test)
   Wire.begin();
@@ -178,6 +269,7 @@ void loop() {
       digitalWrite(PIN_HEATER, LOW);
       analogWrite(PIN_BUZZER, BUZZER_VOL);
       analogWrite(PIN_BLOWER, BLOWER_HIGH);
+      driver.goTo(OPENPOS);
       
       state = 1;
       break;
@@ -191,6 +283,7 @@ void loop() {
       digitalWrite(PIN_HEATER, HIGH);
       analogWrite(PIN_BUZZER, 0);
       analogWrite(PIN_BLOWER, BLOWER_LOW);
+      driver.goTo(CLOSEDPOS);
       
       state = 0;
       break;
